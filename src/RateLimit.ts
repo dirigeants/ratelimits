caed9bfa6473d546414c76bc4ab01596be892667
Gryffon Bellish <owenbellish@gmail.com>
2019-11-11 11:56:22 -0500

    docs: finish docs

--- src/RateLimit.ts
+++ src/RateLimit.ts
@@ -23,18 +23,30 @@ export class RateLimit {
 		this.reset();
 	}
 
+	/**
+	 * Whether this RateLimit is expired or not, allowing the bucket to be reset
+	 */
 	public get expired(): boolean {
 		return this.remainingTime === 0;
 	}
 
+	/**
+	 * Whether this RateLimit is limited or not
+	 */
 	public get limited(): boolean {
 		return !(this.remaining > 0 || this.expired);
 	}
 
+	/**
+	 * The remaining time in milliseconds before this RateLimit instance is reset
+	 */
 	public get remainingTime(): number {
 		return Math.max(this.time - Date.now(), 0);
 	}
 
+	/**
+	 * Drips the RateLimit bucket
+	 */
 	public drip(): this {
 		if (this.limited) throw new Error('Ratelimited');
 		if (this.expired) this.reset();
@@ -43,17 +55,32 @@ export class RateLimit {
 		return this;
 	}
 
+	/**
+	 * Resets the RateLimit back to it's full state
+	 */
 	public reset(): this {
 		return this.resetRemaining().resetTime();
 	}
 
+	/**
+	 * Resets the RateLimit's remaining uses back to full state
+	 */
 	public resetRemaining(): this {
+		/**
+		 * The remaining times this RateLimit can be dripped before the RateLimit bucket is empty
+		 */
 		this.remaining = this.bucket;
 
 		return this;
 	}
 
+	/**
+	 * Resets the RateLimit's reset time back to full state
+	 */
 	public resetTime(): this {
+		/**
+		 * When this RateLimit is reset back to a full state
+		 */
 		this.time = Date.now() + this.cooldown;
 
 		return this;

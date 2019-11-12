2b7e72bc9fdc624455526da0fbe8a8d0a253a86e
Gryffon Bellish <owenbellish@gmail.com>
2019-11-12 10:36:24 -0500

    basic tests, one more to go

--- test/Util.ts
+++ test/Util.ts
@@ -1,4 +1,18 @@
 import ava from 'ava';
-import Util from '../dist';
+import { RateLimitManager } from '../dist';
 
 // ... others
+
+ava('Acquiring', (test): void => {
+	const manager = new RateLimitManager(1, 1);
+
+	const ratelimit = manager.acquire('Hello, world');
+	test.is(ratelimit, manager.get('Hello, world'));
+});
+
+ava('Basic Drip', (test): void => {
+	const manager = new RateLimitManager(2, 30000);
+
+	const ratelimit = manager.acquire('Hello, world');
+	test.throws(ratelimit.drip.bind(ratelimit), 'Ratelimited');
+});

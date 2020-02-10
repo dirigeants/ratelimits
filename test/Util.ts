ffab45ff450e0a0ef9e04fb07394bfd99e3e07e9
bdistin <bdistin@gmail.com>
2020-02-10 10:31:28 -0600

    add more tests

--- test/Util.ts
+++ test/Util.ts
@@ -1,20 +1,59 @@
 import ava from 'ava';
 import { RateLimitManager } from '../dist';
 import { sleep } from '@klasa/utils';
+import Collection from '@discordjs/collection';
 
 // ... others
 
 ava('Acquiring', (test): void => {
+	test.plan(2);
 	const manager = new RateLimitManager(1, 1);
 
-	const ratelimit = manager.acquire('Hello, world');
-	test.is(ratelimit, manager.get('Hello, world'));
+	const ratelimit1 = manager.acquire('one');
+	const ratelimit2 = manager.acquire('two');
+	test.is(ratelimit1, manager.get('one'));
+	test.is(ratelimit2, manager.get('two'));
+});
+
+ava('Set guard', (test): void => {
+	const manager = new RateLimitManager(2, 30000);
+
+	test.throws(() => manager.set('foo', 'bar'), {
+		instanceOf: Error,
+		message: 'Invalid RateLimit'
+	});
+});
+
+ava('Change Bucket', (test): void => {
+	test.plan(4);
+
+	const manager = new RateLimitManager(2, 30000);
+	const ratelimit = manager.acquire('one');
+
+	test.is(manager.bucket, 2);
+	test.is(ratelimit.bucket, 2);
+	manager.bucket = 3;
+	test.is(manager.bucket, 3);
+	test.is(ratelimit.bucket, 3);
+});
+
+ava('Change Cooldown', (test): void => {
+	test.plan(4);
+
+	const manager = new RateLimitManager(2, 30000);
+	const ratelimit = manager.acquire('one');
+
+	test.is(manager.cooldown, 30000);
+	test.is(ratelimit.cooldown, 30000);
+	manager.bucket = 330000;
+	test.is(manager.cooldown, 330000);
+	test.is(ratelimit.cooldown, 330000);
 });
 
 ava('Basic Drip', (test): void => {
 	const manager = new RateLimitManager(2, 30000);
 
-	const ratelimit = manager.acquire('Hello, world');
+	const ratelimit = manager.acquire('one');
 	ratelimit.drip()
 		.drip();
 	test.throws(ratelimit.drip.bind(ratelimit), {
@@ -27,7 +66,7 @@ ava('Proper resetting', async (test): Promise<void> => {
 	test.plan(3);
 	const manager = new RateLimitManager(2, 1000);
 
-	const ratelimit = manager.acquire('Hello, world');
+	const ratelimit = manager.acquire('one');
 	ratelimit.drip()
 		.drip();
 
@@ -37,5 +76,30 @@ ava('Proper resetting', async (test): Promise<void> => {
 	await sleep(1200);
 
 	test.is(ratelimit.limited, false);
-	test.notThrows(() => ratelimit.drip());
+	test.notThrows(ratelimit.drip.bind(ratelimit));
+});
+
+ava('Proper sweeping', async (test): Promise<void> => {
+	test.plan(2);
+	const manager = new RateLimitManager(2, 1000);
+
+	const ratelimit = manager.acquire('one');
+	ratelimit.drip();
+
+	test.is(ratelimit.limited, true);
+
+	// Sleep for 1.2 seconds because of how timers work.
+	await sleep(1200);
+	manager.sweep();
+
+	test.false(manager.has('one'));
+});
+
+ava('Clones are just Collections', async (test): Promise<void> => {
+	test.plan(2);
+
+	const manager = new RateLimitManager(2, 1000);
+
+	test.false(manager.clone() instanceof RateLimitManager);
+	test.true(manager.clone() instanceof Collection);
 });
